c1985616f259225ab0510b62b20a33f6793e462f
Yadeah <m13262298768@163.com>
2020-02-26 19:17:53 +0800

    init

--- src/web/src/router/index.js
+++ src/web/src/router/index.js
@@ -86,7 +86,6 @@ export const constantRoutes = [
  * the routes that need to be dynamically loaded based on user roles
  */
 export const asyncRoutes = [
-
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
 ]
