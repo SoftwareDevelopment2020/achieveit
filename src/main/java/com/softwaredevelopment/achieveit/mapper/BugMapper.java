1901e3f9313816eefc7246e064ec038f0b92edb1
RainkQ <rainkq@gmail.com>
2020-02-27 14:09:43 +0800

    test basemapper

--- src/main/java/com/softwaredevelopment/achieveit/mapper/BugMapper.java
+++ src/main/java/com/softwaredevelopment/achieveit/mapper/BugMapper.java
@@ -2,6 +2,7 @@ package com.softwaredevelopment.achieveit.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.softwaredevelopment.achieveit.entity.Bug;
+import org.springframework.stereotype.Repository;
 
 /**
  * BugMapper
@@ -10,5 +11,6 @@ import com.softwaredevelopment.achieveit.entity.Bug;
  * @version 1.0.0
  * created on 2020-02-27 14:05
  */
+@Repository
 public interface BugMapper extends BaseMapper<Bug> {
 }
