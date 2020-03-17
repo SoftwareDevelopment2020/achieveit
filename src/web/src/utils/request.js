3a4362809d695ada1550048abd8f9bc2ec30891f
tjx <1002376198@qq.com>
2020-03-17 20:28:15 +0800

    前端请求改为http

--- src/web/src/utils/request.js
+++ src/web/src/utils/request.js
@@ -6,10 +6,11 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  // baseURL: process.env.VUE_APP_BASE_API,
-  baseURL: 'https://localhost:8443',
+  baseURL: process.env.VUE_APP_BASE_API,
+  // baseURL: 'https://localhost:8443',
   // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
+  // withCredentials: true,
+  // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
 
