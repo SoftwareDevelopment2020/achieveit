5c8c876e33c8e81d360fede19a1bd9e93692d86b
Yadeah <m13262298768@163.com>
2020-03-19 00:09:02 +0800

    新建项目页面跳转

---
+++ src/web/src/views/project/menu-item/my-project/add.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-page-header @back="goBack" content="立项申请">
+  </el-page-header>
+</template>
+
+<script>
+  export default {
+    methods: {
+      goBack () {
+        this.$emit('changePage')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
