13cd2c7b0efd71f089ed0d73f776f7d9eb613772
Yadeah <m13262298768@163.com>
2020-03-27 21:29:27 +0800

    刷新后，projectId存储问题

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -6,6 +6,17 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  created() {
+    // 页面加载时读取sessionStorage里的项目信息
+    if (sessionStorage.getItem('project')) {
+      this.$store.dispatch('project/setProject',JSON.parse(sessionStorage.getItem('project')))
+    }
+
+    // 页面刷新时将将项目信息存储在sessionStorage中
+    window.addEventListener('beforeunload', () => {
+      sessionStorage.setItem('project', JSON.stringify(this.$store.getters.project))
+    })
+  }
 }
 </script>
