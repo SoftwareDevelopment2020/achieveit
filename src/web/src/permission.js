293f661023d87cb8616a57bc8ddb6336e6078bc2
Yadeah <m13262298768@163.com>
2020-03-27 19:09:07 +0800

    修复路由显示问题

--- src/web/src/permission.js
+++ src/web/src/permission.js
@@ -40,9 +40,9 @@ router.beforeEach(async(to, from, next) => {
           const { roles } = await store.dispatch('user/getInfo')
           // generate accessible routes map based on roles
           // 设定可访问路由信息
-          // const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
           // // dynamically add accessible routes
-          // router.addRoutes(accessRoutes)
+          router.addRoutes(accessRoutes)
 
           // hack method to ensure that addRoutes is complete
           // set the replace: true, so the navigation will not leave a history record
