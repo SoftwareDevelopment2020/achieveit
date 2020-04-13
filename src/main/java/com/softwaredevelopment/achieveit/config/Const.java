498ca075a9990d4ba4461c85a753529f950d856d
RainkQ <rainkq@gmail.com>
2020-04-13 18:27:50 +0800

    token时间

--- src/main/java/com/softwaredevelopment/achieveit/config/Const.java
+++ src/main/java/com/softwaredevelopment/achieveit/config/Const.java
@@ -8,8 +8,8 @@ package com.softwaredevelopment.achieveit.config;
  * created on 2020-02-28 10:42
  */
 public class Const {
-    // 1天(以毫秒ms计)
-    public static final long EXPIRATION_TIME = 86_400_000;
+    // 30天(以毫秒ms计)
+    public static final long EXPIRATION_TIME = 2_592_000_000L;
     // JWT密码
     public static final String SECRET = "achieveitSecret";
     // Token前缀
